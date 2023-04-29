d6bc7affea0b421de102db93da45ae9e5b64aba6
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-04-29 22:54:47 +0300

    path error fixed another

--- js/appController.js
+++ js/appController.js
@@ -3,25 +3,9 @@ let gCanvas
 let gCtx
 
 function onInit() {
-  // Initilize canvas
   gCanvas = document.querySelector('#main-canvas')
   gCtx = gCanvas.getContext('2d')
-  // TODO add to fnction initilize eventlisteners
-  addMouseListeners()
-  addTouchListeners()
   window.addEventListener('resize', resizeCanvas)
   renderGallerySection()
   renderEmojis()
-}
-
-function addMouseListeners() {
-  gCanvas.addEventListener('mousedown', onDown)
-  // gCanvas.addEventListener('mousemove', onMove)
-  // gCanvas.addEventListener('mouseup', onUp)
-}
-
-function addTouchListeners() {
-  gCanvas.addEventListener('touchstart', onDown)
-  // gCanvas.addEventListener('touchmove', onMove)
-  // gCanvas.addEventListener('touchend', onUp)
 }
